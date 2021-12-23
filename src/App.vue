a903eb5cba73a5402ffb20b0d14023da3299ddee
Dennis Mullen <dkmullen@gmail.com>
2021-12-22 19:45:25 -0500

    Add nav drawer

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,69 @@
 <template>
-	<v-app>
-		<v-app-bar app color="primary" dark>
-			<div class="d-flex align-center">
-				<router-link to="/">
-					<v-img
-						alt="Cupcake Logo"
-						class="shrink mr-2"
-						contain
-						src="./assets/cupcake.png"
-						transition="scale-transition"
-						width="40"
-					/>
-				</router-link>
-				<div class="logo-text">Vue Demo</div>
-			</div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <router-link to="/">
+          <v-img
+            alt="Cupcake Logo"
+            class="shrink mr-2"
+            contain
+            src="./assets/cupcake.png"
+            transition="scale-transition"
+            width="40"
+          />
+        </router-link>
+        <div class="logo-text">Vue Demo</div>
+      </div>
 
-			<v-spacer></v-spacer>
-			<router-link class="header-link mr-1" to="/">Home</router-link> |
-			<router-link class="header-link ml-1 mr-4" to="/about">About</router-link>
+      <v-spacer></v-spacer>
+      <!-- <router-link class="header-link mr-1" to="/">Home</router-link> |
+      <router-link class="header-link ml-1 mr-4" to="/about">About</router-link> -->
 
-			<v-btn @click="toggleTheme" class="theme-btn" text>
-				<span class="mr-2">{{
-					!$vuetify.theme.dark ? 'Go dark' : 'Go light'
-				}}</span>
-				<v-icon>{{
-					!$vuetify.theme.dark ? 'mdi-toggle-switch-off' : 'mdi-toggle-switch'
-				}}</v-icon>
-			</v-btn>
-		</v-app-bar>
-
-		<v-main>
-			<router-view />
-		</v-main>
-	</v-app>
+      <v-btn @click="toggleTheme" class="theme-btn" text>
+        <span class="mr-2">{{
+          !$vuetify.theme.dark ? 'Go dark' : 'Go light'
+        }}</span>
+        <v-icon>{{
+          !$vuetify.theme.dark ? 'mdi-toggle-switch-off' : 'mdi-toggle-switch'
+        }}</v-icon>
+      </v-btn>
+      <v-app-bar-nav-icon @click="toggleNav"></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-main>
+      <nav-drawer ref="drawer"> </nav-drawer>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
+import NavDrawer from './components/NavDrawer.vue';
+
 export default {
-	name: 'App',
-	methods: {
-		toggleTheme() {
-			this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
-		},
-	},
+  name: 'App',
+  components: { NavDrawer },
+  data() {
+    return {};
+  },
+  methods: {
+    toggleTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+    toggleNav() {
+      this.$refs.drawer.toggleDrawer();
+    },
+  },
 };
 </script>
 <style lang="scss">
 .logo-text {
-	font-size: 2rem;
-	font-weight: 300;
-	font-variant: small-caps;
+  font-size: 2rem;
+  font-weight: 300;
+  font-variant: small-caps;
 }
 .header-link {
-	color: white !important;
+  color: white !important;
 }
 .theme-btn {
-	width: 140px;
+  width: 140px;
 }
 </style>
---
+++ src/components/NavDrawer.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-sheet class="overflow-hidden" style="position: relative">
+    <v-container class="fill-height">
+      <router-view />
+    </v-container>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary right>
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>John Leider</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          @click="goTo(item.link)"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: null,
+      items: [
+        { title: 'Home', icon: 'mdi-home', link: 'Home' },
+        { title: 'About', icon: 'mdi-information', link: 'About' },
+        { title: 'Weather', icon: 'mdi-umbrella', link: 'Home' },
+        { title: 'Run Calculator', icon: 'mdi-run', link: 'Home' },
+      ],
+    };
+  },
+  methods: {
+    toggleDrawer() {
+      this.drawer = !this.drawer;
+    },
+    goTo(link) {
+      this.$router.push({ name: link });
+    },
+  },
+  // props: {
+  //   drawer: { type: Boolean, default: false },
+  // },
+};
+</script>
+
+<style></style>
