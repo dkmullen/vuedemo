79c348e49a1c548c35b83428705ec2dbef1636aa
Dennis Mullen <dkmullen@gmail.com>
2021-12-24 11:12:25 -0500

    Format cards, play with time zones

--- src/components/widgets/WeatherCard.vue
+++ src/components/widgets/WeatherCard.vue
@@ -1,6 +1,37 @@
 <template>
   <div>
     <v-card class="mx-auto" max-width="400" outlined v-if="weatherObj">
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title class="text-h5"
+            >{{ weatherObj.name }}
+          </v-list-item-title>
+          <v-list-item-subtitle
+            >{{ weatherObj.time }},
+            <span class="text-capitalize">{{ weatherObj.condition }}</span>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-card-text>
+        <v-row align="center">
+          <v-col class="text-h2" cols="6">
+            {{ weatherObj.temperature }}&deg;F
+          </v-col>
+          <v-col cols="6">
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/sun.png"
+              alt="Sunny image"
+              width="92"
+            ></v-img>
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-cloud-download</v-icon>
+        </v-list-item-icon>
+        <v-list-item-subtitle>{{ weatherObj.humidity }}%</v-list-item-subtitle>
+      </v-list-item>
       <div v-for="item in weatherObj" :key="item">
         {{ item }}
       </div>
@@ -20,6 +51,7 @@ export default {
   data() {
     return {
       weatherObj: null,
+      now: null,
     };
   },
   methods: {
@@ -38,12 +70,31 @@ export default {
           condition: response.data.weather[0].description,
           humidity: response.data.main.humidity,
           timezone: response.data.timezone,
+          time: this.now + response.data.timezone,
         };
       }
+    },
+    calcTime(city, offset) {
+      // create Date object for current location
+      let d = new Date();
+      console.log(d.getTime(), d.getTimezoneOffset());
+      // convert to msec
+      // add local time zone offset
+      // get UTC time in msec
+      let utc = d.getTime() + d.getTimezoneOffset() * 600000;
+      console.log(utc);
+      // create new Date object for different city
+      // using supplied offset
+      let nd = new Date(d + 3600000 * offset);
+
+      // return time as a string
+      console.log('The local time in ' + city + ' is ' + nd.toLocaleString());
     },
   },
   created() {
     this.getWeather();
+    this.now = Date.now();
+    this.calcTime('Denver', 1);
   },
 };
 </script>
